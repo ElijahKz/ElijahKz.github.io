1ef80dc3b7e2306659fbbe6a2c7d5b482b142dc8
Dark <milmax.duque@uao.edu.co>
2020-06-08 10:49:14 -0500

    proyecto

---
+++ precache-manifest.23ee49b1c2b20913213ebed49ca1be31.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64910881067fc74ca370bc141478c76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "680d0e7c354cafc3d6b2",
+    "url": "/static/css/main.48808b1e.chunk.css"
+  },
+  {
+    "revision": "82b92480befbaf72a861",
+    "url": "/static/js/2.0479b97d.chunk.js"
+  },
+  {
+    "revision": "ce7bc576c2c75879a1a18bc916de373e",
+    "url": "/static/js/2.0479b97d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "680d0e7c354cafc3d6b2",
+    "url": "/static/js/main.44af2931.chunk.js"
+  },
+  {
+    "revision": "3d0b735d4098ab10bab1",
+    "url": "/static/js/runtime-main.2bb40d1a.js"
+  },
+  {
+    "revision": "85e35579e5b73774e24a335947664656",
+    "url": "/static/media/ManosFull.85e35579.svg"
+  },
+  {
+    "revision": "f29b52575ce1a676d70a19d4ab0ef2d5",
+    "url": "/static/media/R1.f29b5257.png"
+  },
+  {
+    "revision": "4bb2e273b86c156e2ad17cdeec5366c5",
+    "url": "/static/media/R2.4bb2e273.png"
+  },
+  {
+    "revision": "0ee13e2452af51f87a5dbdd1871d69bd",
+    "url": "/static/media/R3.0ee13e24.png"
+  },
+  {
+    "revision": "bc4188624bc9e06022dd54f95d9d8e84",
+    "url": "/static/media/R4.bc418862.png"
+  },
+  {
+    "revision": "85efd651744574bd0fcc19e6b7c0eadb",
+    "url": "/static/media/R5.85efd651.png"
+  },
+  {
+    "revision": "06ee020616b3462a6b97233611e9b00e",
+    "url": "/static/media/R6.06ee0206.png"
+  },
+  {
+    "revision": "6f9e7b59d699b2549c2fa245db0f1a5a",
+    "url": "/static/media/background1.6f9e7b59.svg"
+  },
+  {
+    "revision": "5ad0b408a734a9cfe4b44c8b4de3b9e6",
+    "url": "/static/media/background2.5ad0b408.svg"
+  },
+  {
+    "revision": "1f96be121d9763bec477942cd2ec5e65",
+    "url": "/static/media/barco-step-a.1f96be12.svg"
+  },
+  {
+    "revision": "603e002db0971047bbfd99300310feb6",
+    "url": "/static/media/barco-step-b.603e002d.svg"
+  },
+  {
+    "revision": "346c60562ae75249066ee2b0cd459f8d",
+    "url": "/static/media/barco-step-c.346c6056.svg"
+  },
+  {
+    "revision": "3aff1eecb0977b86688bb159d2369ea7",
+    "url": "/static/media/barco-step-d.3aff1eec.svg"
+  },
+  {
+    "revision": "4f159159bbedbd8c1dbcd748592fb741",
+    "url": "/static/media/barco-step-e.4f159159.svg"
+  },
+  {
+    "revision": "9f7a52fa7a3edd7d65ca1d488784d25b",
+    "url": "/static/media/barco-step-f.9f7a52fa.svg"
+  },
+  {
+    "revision": "42d918547203752a08724f5ca570e17e",
+    "url": "/static/media/barco-step-g.42d91854.svg"
+  },
+  {
+    "revision": "bbc580c2034abb6d223aa03086f1dd90",
+    "url": "/static/media/barco-step-h.bbc580c2.svg"
+  },
+  {
+    "revision": "ee85963226d4c7e520b52733f2fec526",
+    "url": "/static/media/barco-step-i.ee859632.svg"
+  },
+  {
+    "revision": "e85792947766cc444fc6237970d6ad21",
+    "url": "/static/media/barco-step-j.e8579294.svg"
+  },
+  {
+    "revision": "adc63588f6feb927c5210e7002bb5bec",
+    "url": "/static/media/barco-step-k.adc63588.svg"
+  },
+  {
+    "revision": "29b7296d0bafba06ea21a7dc9b59220e",
+    "url": "/static/media/barco-step-l.29b7296d.svg"
+  },
+  {
+    "revision": "9f7bbc29d7709bdb2ea65794026d8c06",
+    "url": "/static/media/barcofull.9f7bbc29.svg"
+  },
+  {
+    "revision": "ad5f429d9b93aa8892dfa21cc2b9dbc8",
+    "url": "/static/media/cuadros.ad5f429d.svg"
+  },
+  {
+    "revision": "010932e6714cceaa370daec8cdb95c79",
+    "url": "/static/media/cuadros2.010932e6.svg"
+  },
+  {
+    "revision": "2daabb7a799eca64ba4e56e96266654d",
+    "url": "/static/media/cuadros3.2daabb7a.svg"
+  },
+  {
+    "revision": "fd94c313599b1a404ad7e4d859343569",
+    "url": "/static/media/fig1.fd94c313.jpg"
+  },
+  {
+    "revision": "72656c5dbc1b576a771f039d22b8d02e",
+    "url": "/static/media/figura.72656c5d.svg"
+  },
+  {
+    "revision": "4abf44421e24fc28a8aa5578b690c655",
+    "url": "/static/media/fondo.4abf4442.jpg"
+  },
+  {
+    "revision": "f6cd9ced1588483a02d2fbf23399a7bb",
+    "url": "/static/media/fondo2.f6cd9ced.jpg"
+  },
+  {
+    "revision": "71f03a9f94f0140228dbd230a62dc8e9",
+    "url": "/static/media/fondo3.71f03a9f.jpg"
+  },
+  {
+    "revision": "f57e3d67b0876c6ccaaf5a065dbab229",
+    "url": "/static/media/manos-step-a.f57e3d67.svg"
+  },
+  {
+    "revision": "c31ebc5874780308bc0e267d828d818b",
+    "url": "/static/media/manos-step-b.c31ebc58.svg"
+  },
+  {
+    "revision": "6c2dc78bdbb326a053214247c6463e81",
+    "url": "/static/media/manos-step-c.6c2dc78b.svg"
+  },
+  {
+    "revision": "0752b0fbff09c30b1e73a450d11afca9",
+    "url": "/static/media/manos-step-d.0752b0fb.svg"
+  },
+  {
+    "revision": "6f06a7ca5c28603fc890dc42597e5719",
+    "url": "/static/media/manos-step-e.6f06a7ca.svg"
+  },
+  {
+    "revision": "be6fed4d4e3d8a1a5a7984d0f02f945d",
+    "url": "/static/media/manos-step-f.be6fed4d.svg"
+  },
+  {
+    "revision": "edbda0a6a9626e3303cf5f5cdf8cf456",
+    "url": "/static/media/manos-step-g.edbda0a6.svg"
+  },
+  {
+    "revision": "5fccf7e833164ca9432930b7ff53e156",
+    "url": "/static/media/manos-step-h.5fccf7e8.svg"
+  },
+  {
+    "revision": "d1a2afa50c34c5fccbf7d132cd8143ad",
+    "url": "/static/media/manos-step-i.d1a2afa5.svg"
+  },
+  {
+    "revision": "66ae473d84b84e6ab1f78582de7b8fd1",
+    "url": "/static/media/manos-step-j.66ae473d.svg"
+  }
+]);
